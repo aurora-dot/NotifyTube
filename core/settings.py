be5af1c12754011218430d9359339787a27dacbf
E <e@pastel.codes>
2024-06-28 13:21:30 +0100

    Add pylint disable for line which can't be formatted

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # pylint: disable=C0301
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
