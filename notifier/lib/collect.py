ea0850cec047e3054fbb869b76e4ca8c8b8211d2
E <e@pastel.codes>
2024-07-08 16:19:24 +0100

    WIP: Get videos from youtube and extract data

--- notifier/lib/collect.py
+++ notifier/lib/collect.py
@@ -5,18 +5,25 @@ Collects YouTube video data.
 import time
 from urllib.parse import quote_plus
 
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from django.conf import settings
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
+from notifier.lib.metadata_extractor import MetadataExtractor
 
 
 class Collector:
     """
     Collects YouTube video data scraped from their webpage.
     """
+
+    youtube_video_tag = "ytd-video-renderer"
+    extractor = MetadataExtractor()
 
     def run(self, search_query: str, last_video_id: str) -> list:
         """
@@ -32,10 +39,78 @@ class Collector:
         Raises:
             ValueError: if the parameters to the function are none or an empty string
         """
+        return self._get_newest_videos_data(search_query, last_video_id)
 
-        pass
+    def get_initial_video_for_query(self, search_query) -> list[list, str]:
+        browser = self._goto_query_page(search_query)
+        first_video = browser.find_element(By.TAG_NAME, self.youtube_video_tag)
+        return self.extractor.extract(first_video)
 
-    def _setup_browser(self):
+    def _goto_query_page(self, search_query):
+        browser = self._setup_browser()
+        browser.get(
+            f"https://www.youtube.com/results?search_query={quote_plus(search_query)}&sp=CAI%253D"  # pylint: disable=C0301
+        )
+        self._close_cookie_popup(browser)
+
+        return browser
+
+    def _search_and_scroll(self, search_query: str, last_video_id: str) -> WebDriver:
+        browser = self._goto_query_page(search_query)
+
+        for _ in range(60):
+            # currently jank just to get it working,
+            # scrolling to body position doesn't work
+            browser.execute_script(
+                "window.scrollTo(0, 99999999999999999999999999999999)"
+            )
+            time.sleep(0.5)
+
+            if self._element_exists(
+                browser, f'//a[contains(@href ,"{last_video_id}")]'
+            ):
+                break
+
+            if self._element_exists(
+                browser, '//yt-formatted-string[contains(text(), "No more results")]'
+            ):
+                raise LookupError("Could not find last video id from query")
+
+        if settings.DEBUG:
+            with open("page.html", "w", encoding="utf-8") as file:
+                file.write(browser.page_source)
+
+        return browser
+
+    def _get_newest_videos_data(self, search_query: str, last_video_id: str):
+        browser = self._search_and_scroll(search_query, last_video_id)
+        videos = browser.find_elements(By.TAG_NAME, self.youtube_video_tag)
+        return [self.extractor.extract(video) for video in videos]
+
+    @staticmethod
+    def _element_exists(browser: WebDriver, xpath_string: str):
+        try:
+            browser.find_element(By.XPATH, xpath_string)
+        except NoSuchElementException:
+            return False
+        return True
+
+    @staticmethod
+    def _close_cookie_popup(browser: WebDriver):
+        cookie_decline_xpath = '//*[@id="content"]/div[2]/div[6]/div[1]/ytd-button-renderer[1]/yt-button-shape/button'  # pylint: disable=C0301
+
+        try:
+            WebDriverWait(browser, 25).until(
+                EC.presence_of_element_located((By.XPATH, cookie_decline_xpath))
+            )
+        except TimeoutException as error:
+            browser.quit()
+            raise error
+
+        browser.find_element(By.XPATH, cookie_decline_xpath).click()
+
+    @staticmethod
+    def _setup_browser() -> WebDriver:
         chrome_options = Options()
         # chrome_options.add_argument("--headless=true")
         # chrome_options.add_argument("--window-size=1920x1080")
@@ -49,41 +124,4 @@ class Collector:
         # chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")
         # chrome_options.add_argument("--remote-debugging-port=9222")
 
-        return webdriver.Chrome(options=chrome_options)
-
-    def get_initial_video_for_query(self, search_query) -> list[list, str]:
-        pass
-
-    def _search(self, search_query: str, last_video_id: str) -> list:
-        browser = self._setup_browser()
-        browser.get(
-            f"https://www.youtube.com/results?search_query={quote_plus(search_query)}&sp=CAI%253D"
-        )
-        cookie_decline_xpath = '//*[@id="content"]/div[2]/div[6]/div[1]/ytd-button-renderer[1]/yt-button-shape/button'
-
-        try:
-            WebDriverWait(browser, 25).until(
-                EC.presence_of_element_located((By.XPATH, cookie_decline_xpath))
-            )
-        except TimeoutException as error:
-            browser.quit()
-            raise error
-
-        browser.find_element(By.XPATH, cookie_decline_xpath).click()
-        time.sleep(1)
-
-        # this should be dynamic, scrolling the the previous video id and stopping when it hits it
-        for _ in range(60):
-            # currently jank just to get it working, scrolling to body position doesn't work
-            browser.execute_script(
-                "window.scrollTo(0, 99999999999999999999999999999999)"
-            )
-            time.sleep(0.5)
-
-            # if //*[@id="message"] appears, we have reached bottom
-
-        with open("page.html", "w", encoding="utf-8") as file:
-            file.write(browser.page_source)
-
-    def _transform_data(self, youtube_list: list, youtube_stats_list: list) -> list:
-        pass
+        return Chrome(options=chrome_options)
---
+++ notifier/lib/metadata_extractor.py
@@ -0,0 +1,52 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+
+class MetadataExtractor:
+    def extract(self, video_element: WebElement):
+        return (
+            self._get_link_data(video_element)
+            | self._get_title_and_thumbnail(video_element)
+            | self._get_channel_data(video_element)
+        )
+
+    def _get_title_and_thumbnail(self, video_element):
+        title = (
+            video_element.find_element(By.ID, "video-title")
+            .find_element(By.TAG_NAME, "yt-formatted-string")
+            .text
+        )
+        thumbnail = (
+            video_element.find_element(By.ID, "thumbnail")
+            .find_element(By.TAG_NAME, "img")
+            .get_attribute("src")
+        )
+
+        return {"title": title, "thumbnail": thumbnail}
+
+    def _get_link_data(self, video_element):
+        url_prefix = "https://www.youtube.com/watch?v="
+
+        dirty_link = video_element.find_element(By.ID, "thumbnail").get_attribute(
+            "href"
+        )
+        cleaned_link = dirty_link.split("&", 1)[0]
+        video_id = cleaned_link.removeprefix(url_prefix)
+        if url_prefix in video_id:
+            raise ValueError("video_id still contains url prefix")
+
+        return {"link": cleaned_link, "video_id": video_id}
+
+    def _get_channel_data(self, video_element):
+        channel_img = video_element.find_element(By.ID, "img").get_attribute("src")
+        channel_name_element = video_element.find_element(
+            By.ID, "channel-name"
+        ).find_element(By.TAG_NAME, "a")
+        channel_name = channel_name_element.text
+        channel_link = channel_name_element.get_attribute("href")
+
+        return {
+            "channel_img": channel_img,
+            "channel_name": channel_name,
+            "channel_link": channel_link,
+        }
