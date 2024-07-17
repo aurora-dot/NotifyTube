05a450ebbbd2fd066ea5e660165dd9eea64620d1
E <e@pastel.codes>
2024-07-17 12:07:01 +0100

    Update theming

--- app/forms.py
+++ app/forms.py
@@ -24,7 +24,7 @@ class YouTubeEmailSubscription(forms.ModelForm):
         required=True,
         widget=forms.EmailInput(
             attrs={
-                "class": "w-full border-0 rounded-full p-2 pl-3 pl-r ring-1 ring-inset ring-gray-500 focus:ring-purple-500 hover:ring-purple-500 placeholder:text-gray-400 focus:ring-2 focus:ring-inset"
+                "class": "text-sm w-full border-0 rounded-full p-2 pl-3 pl-r ring-1 ring-inset ring-gray-300 focus:ring-purple-500 hover:ring-purple-500 placeholder:text-gray-400 focus:ring-2 focus:ring-inset"
             }
         ),
     )
@@ -34,5 +34,10 @@ class YouTubeEmailSubscription(forms.ModelForm):
         fields = ["email_frequency"]
         labels = {"email_frequency": "Frequency"}
         widgets = {
-            "email_frequency": forms.Select(attrs={"class": "", "required": True})
+            "email_frequency": forms.Select(
+                attrs={
+                    "class": "block w-full pl-3 pr-10 py-1.5 text-sm border-0 ring-gray-300 focus:outline-none hover:ring-purple-500 focus:ring-purple-500 ring-1 focus:ring-2 rounded-md",
+                    "required": True,
+                }
+            )
         }
