60183c51c96eccd4c168704c54529ace4c119d0d
E <e@pastel.codes>
2024-07-19 20:53:23 +0100

    Add another chrome option

--- notifier/lib/collect.py
+++ notifier/lib/collect.py
@@ -195,6 +195,7 @@ class Collector:
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument(
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"  # pylint: disable=C0301
         )
