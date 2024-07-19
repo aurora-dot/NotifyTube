241bf065f986caf1fec8ebb0cb86487d6a93e307
E <e@pastel.codes>
2024-07-19 20:49:01 +0100

    Add no-sandbox

--- notifier/lib/collect.py
+++ notifier/lib/collect.py
@@ -192,7 +192,8 @@ class Collector:
     def _setup_browser() -> WebDriver:
         chrome_options = Options()
         chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.add_argument("--headless=true")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument(
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"  # pylint: disable=C0301
