0578f0ab6f49a6cbf7e4e9724937d982641ab777
E <e@pastel.codes>
2024-07-19 11:31:29 +0100

    Add logging to notifier

--- notifier/lib/notify.py
+++ notifier/lib/notify.py
@@ -1,24 +1,35 @@
+from datetime import datetime
+
 from django.conf import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
+from notifier.lib.logger import LOGGER
 from notifier.models import Subscription, SubscriptionEmailFrequency, YouTubeVideo
 
 
 def notify_hourly():
+    LOGGER.info("Notifier - %s: Started hourly", datetime.now())
     notify_common(SubscriptionEmailFrequency.HOURLY)
 
 
 def notify_daily():
+    LOGGER.info("Notifier - %s: Started hourly", datetime.now())
     notify_common(SubscriptionEmailFrequency.DAILY)
 
 
 def notify_weekly():
+    LOGGER.info("Notifier - %s: Started hourly", datetime.now())
     notify_common(SubscriptionEmailFrequency.WEEKLY)
 
 
 def notify_common(freq):
     for subscription in Subscription.objects.filter(email_frequency=freq):
+        LOGGER.info(
+            "Notifier - %s: Emailing subscribers for query %s",
+            datetime.now(),
+            subscription.query.query,
+        )
         email = subscription.email.email
         query = subscription.query
         last_sent = subscription.last_sent
@@ -43,4 +54,9 @@ def notify_common(freq):
                 [email],
                 html_message=message,
             )
-            # query.save()
+            query.save()
+            LOGGER.info(
+                "Notifier - %s: Sent emails for query %s",
+                datetime.now(),
+                subscription.query.query,
+            )
